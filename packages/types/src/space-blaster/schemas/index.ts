77d6afd50bfab43f81e02096d6785e951ef45ff7
Nico Pretorius <info@nicopretorius.com>
2026-02-11 09:10:40 +0000

    Task 12.3 - Implement LevelConfig JSON schema

--- packages/types/src/space-blaster/schemas/index.ts
+++ packages/types/src/space-blaster/schemas/index.ts
@@ -1,7 +1,9 @@
-import gameConigSchema from './game-config.schema.json';
+import gameConfigSchema from './game-config.schema.json';
+import levelConfigSchema from './level-config.schema.json';
 import scoreConfigSchema from './score-config.schema.json';
 
 export const SpaceBlasterSchema = {
-    gameConfig: gameConigSchema,
+    gameConfig: gameConfigSchema,
+    levelConfig: levelConfigSchema, 
     scoreConfig: scoreConfigSchema
 } as const;
