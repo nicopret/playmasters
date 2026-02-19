67f9e7952d3d5d33e64558653b27c62d71a8363e
Nico Pretorius <info@nicopretorius.com>
2026-02-19 10:59:56 +0000

    Implement the first stab at the asset page in the admin client (#238)
    * Implement the first stab at the asset page in the admin client
    
    * Fix github errors

---
+++ apps/admin/src/app/score-config/page.tsx
@@ -0,0 +1,5 @@
+import { redirect } from 'next/navigation';
+
+export default function LegacyScoreConfigPage() {
+  redirect('/games/space-blaster/score-config');
+}
---
+++ apps/admin/src/app/space-blaster/history/page.tsx
@@ -0,0 +1,5 @@
+import { redirect } from 'next/navigation';
+
+export default function LegacySpaceBlasterHistoryPage() {
+  redirect('/games/space-blaster/history');
+}
